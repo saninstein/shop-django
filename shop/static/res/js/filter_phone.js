db4637ad9d676832f68ad4c1feec86c5acef8094
Alex <saninstein@yandex.ua>
2016-07-16 12:54:02 +0300

    0.2

---
+++ shop/static/res/js/filter_phone.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Alex on 16.07.2016.
+ */
+
+var arr = [];
+var i = 0;
+$(":checkbox").change(function(){
+    if(this.checked){
+        arr[i] = $(this).prop("name");
+        i++;
+    }else{
+        var val = $(this).prop("name");
+        var index = arr.indexOf(val);
+        arr.splice(index, 1);
+        i--;
+    }
+    $(".wrp").empty();
+});
