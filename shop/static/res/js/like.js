481e06c87d34e66f0c6c83ddac94d4169db6d845
saninstein <samsung98>
2016-07-18 15:47:13 +0300

    new 0.2

--- shop/static/res/js/like.js
+++ shop/static/res/js/like.js
@@ -1,3 +1,9 @@
 /**
  * Created by шаша on 18.07.2016.
  */
+$(document).ready(function(){
+    if($.cookie('liked') == null){
+        alert("as");
+        $('.fa-heart').css("color", "#FFE566");
+    }
+});
