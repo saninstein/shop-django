b0abeae9b9be31539652212b9a7a1292ac174677
saninstein <samsung98>
2016-07-19 21:27:57 +0300

    new 0.2

--- adminpanel/url.py
+++ adminpanel/url.py
@@ -3,5 +3,5 @@ from adminpanel import views
 
 urlpatterns = [
     url(r'^$', views.general),
-    url(r'^new//(?P<category>[\w\-]+)/$', views.new_item, name='new_item'),
+    url(r'^new/(?P<category>[\w\-]+)/$', views.new_item, name='new_item'),
 ]
