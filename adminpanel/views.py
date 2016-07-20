64d2a8bb0c1a7cc48ae46e4a14d8f4d5950400f6
saninstein <samsung98>
2016-07-20 20:48:06 +0300

    new 0.2

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, render_to_response, redirect
+from django.core.urlresolvers import reverse
 from django.core.context_processors import csrf
 from adminpanel.form import PhoneForm
 from django.http import HttpResponse
@@ -6,16 +7,18 @@ from django.http import HttpResponse
 
 def general(req):
     args = dict()
-    args['gen'] = 'HELLO'
     return render_to_response('admin_general/index.html', args)
 
 
 def new_item(req, category=''):
     args = dict()
     args.update(csrf(req))
+    args['url'] = reverse('new_item', kwargs={'category': 'phone'})
     if req.method == 'POST':
         if category == 'phone':
             form = PhoneForm(req.POST, req.FILES)
+            print(req.FILES)
+
             if form.is_valid():
                 form.save()
                 return redirect('general')
