cb7b3f39090ca5270bd241c80789458fb799185a
saninstein <samsung98>
2016-07-20 22:01:39 +0300

    new 0.2

--- adminpanel/url.py
+++ adminpanel/url.py
@@ -4,4 +4,5 @@ from adminpanel import views
 urlpatterns = [
     url(r'^$', views.general),
     url(r'^new/(?P<category>[\w\-]+)/$', views.new_item, name='new_item'),
+    url(r'^login/', views.login, name='login'),
 ]
--- adminpanel/views.py
+++ adminpanel/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, render_to_response, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import user_passes_test, permission_required
 from django.core.urlresolvers import reverse
 from django.core.context_processors import csrf
 from adminpanel.form import PhoneForm, TabletForm, NotebookForm
 from django.http import HttpResponse
 
 
+def is_su(user):
+    return user.is_superuser
+
+
+@user_passes_test(is_su, login_url=reverse('login'))
 def general(req):
     args = dict()
     return render_to_response('admin_general/index.html', args)
 
 
+@user_passes_test(is_su, login_url=reverse('login'))
 def new_item(req, category=''):
     args = dict()
     args.update(csrf(req))
@@ -51,4 +59,18 @@ def new_item(req, category=''):
     return render_to_response('new_item/index.html', args)
 
 
-
+def login(req):
+    args = {}
+    args.update(csrf(req))
+    if req.POST:
+        username = req.POST.get('username', '')
+        password = req.POST.get('password', '')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(req, user)
+            return redirect('general')
+        else:
+            args['login_error'] = "Пользователь не найден"
+            return render_to_response('login/index.html', args)
+    else:
+        return render_to_response('login/index.html', args)
