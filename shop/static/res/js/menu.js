665ec7f467c19263d1d00bf71baa361ed56349db
saninstein <samsung98>
2016-07-23 16:59:12 +0300

    new 0.2

--- shop/static/res/js/menu.js
+++ shop/static/res/js/menu.js
@@ -1,3 +1,22 @@
-$('#pod-menu').load(function () {
-    $(this).hide();
+$(document).ready(function () {
+    $('#menu li').mouseenter(function () {
+        $('#pod-menu').show();
+        $('#pod-menu li').css({'visibility': 'hidden'});
+        $($(this).data('pd')).css({'visibility': 'visible'});
+        if($(this).data('pd') == '#pd4' || $(this).data('pd') == '#pd5'){
+            $('.in-list ul li').css({'visibility': 'visible'});
+            if($(this).data('pd') == '#pd4'){
+                $('#pd5 .in-list ul li').css({'visibility': 'hidden'});
+            } else {
+                $('#pd4 .in-list ul li').css({'visibility': 'hidden'});
+            }
+        } else{
+            $('#body').mouseenter(function () {
+                $('#pod-menu').hide();
+            });
+        }
+
+
+    });
+
 });
